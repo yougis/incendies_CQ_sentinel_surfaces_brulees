eb3240d28e77635930ce4a951330517a4598a121
Oriane <informatique@oeil.nc>
2024-07-31 14:26:39 +1100

    add : récupération d'un fichier txt listant toutes les tuiles pour l'année sur le serveur insight, utile pour le dashboard de contrôle

--- Recup_Data_INSIGHT/recup_surfaces_brulees_STL2_INSIGHT.sh
+++ Recup_Data_INSIGHT/recup_surfaces_brulees_STL2_INSIGHT.sh
@@ -7,9 +7,11 @@ backup_function() {
     server_user="oeil"
     server_ip="192.168.13.43"
     server_path="../../mnt/geo-storage/FEUXV2/RESULTS/"
+    tile_path="../..//mnt/geo-storage/NC_S2_L2A-THEIA_"$year"/"
     
     # Utilisez rsync pour la sauvegard
     sshpass -p '03il2023!?' rsync -avz --no-perms -e ssh "$server_user@$server_ip:$server_path/*.gpkg" "$rep_loc"
+    sshpass -p '03il2023!?' ssh "$server_user@$server_ip" "ls $tile_path/*.zip" > "$rep_loc/list_of_tiles.txt"
 }
 # Appeler la fonction de sauvegarde
 backup_function
